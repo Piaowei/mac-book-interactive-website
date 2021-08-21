2c63e9502f71eecfd50e5cfb329c30d23633de02
Piaowei <msptonmoy@gmail.com>
2021-08-21 17:34:15 +0600

    finished

--- js/mac-book.js
+++ js/mac-book.js
@@ -1,17 +1,13 @@
 function memoryCost(isprice) {
 	if (isprice) {
-		// const memoryDisplayCost = parseInt(document.getElementById("8gb-amount").innerText);
 		document.getElementById("memory-amount").innerText = parseInt(0);
-
 	}
 	else {
 		document.getElementById("memory-amount").innerText = parseInt(180);
-
 	}
-
 }
 
-// 
+
 function storgeCost(isStorge, anotherStorge) {
 	if (isStorge) {
 		document.getElementById("storge-amount").innerText = parseInt(180);
@@ -19,11 +15,11 @@ function storgeCost(isStorge, anotherStorge) {
 	else if (isStorge == false) {
 		document.getElementById("storge-amount").innerText = parseInt(100);
 	}
-
 	if (isStorge == false && anotherStorge == true) {
 		document.getElementById("storge-amount").innerText = parseInt(0);
 	}
 }
+
 
 function deleveryCost(delevered) {
 	if (delevered) {
@@ -34,16 +30,17 @@ function deleveryCost(delevered) {
 	}
 }
 
+
 function totalPrice() {
+	const bestPrice = parseInt(document.getElementById("best-amount").innerText)
 	const memoryCost = parseInt(document.getElementById("memory-amount").innerText);
 	const storgeCost = parseInt(document.getElementById("storge-amount").innerText);
 	const deleveryCost = parseInt(document.getElementById("delevery-amount").innerText);
-	let totalPrice = parseInt(1299 + memoryCost + storgeCost + deleveryCost);
+	let totalPrice = parseInt(bestPrice + memoryCost + storgeCost + deleveryCost);
 	document.getElementById("total-amount").innerText = totalPrice;
 	document.getElementById("last-amount").innerText = totalPrice;
-
-
 }
+
 
 function cuponPrice() {
 	cuponValue = document.getElementById("cupon-input");
@@ -58,26 +55,24 @@ function cuponPrice() {
 }
 
 
-// even handler memory 8gb
-// ------------------------
+/* ------------------------------------------
+				even handler 
+ ------------------------------------------*/
 
+// even handler memory 8gb
 document.getElementById("8gb-memory").addEventListener("click", function () {
 	memoryCost(true);
 	totalPrice();
-
-
-
 })
-// even handler memory 16gb
 
+
+// even handler memory 16gb
 document.getElementById("16gb-memory").addEventListener("click", function () {
 	memoryCost(false);
 	totalPrice();
-
-
 })
 
-// -----------
+
 // even handler storge 256gb SSD
 document.getElementById("256gb-storge-btn").addEventListener("click", function () {
 	storgeCost(false, true);
@@ -90,25 +85,30 @@ document.getElementById("512gb-storge-btn").addEventListener("click", function (
 	storgeCost(false);
 	totalPrice();
 })
+
+
 // even handler storge 1TB SSD
 document.getElementById("1tb-storge-btn").addEventListener("click", function () {
 	storgeCost(true);
 	totalPrice();
-
 })
 
-// even handler Delevery
 
+// even handler Delevery Button
 document.getElementById("free-delevery-btn").addEventListener("click", function () {
 	deleveryCost(true);
 	totalPrice();
 })
+
+
+// even handler Delevery cost
 document.getElementById("costly-delevery-btn").addEventListener("click", function () {
 	deleveryCost(false);
 	totalPrice();
 })
 
+
+// even handler Cupon cost
 document.getElementById("cupon-btn").addEventListener("click", function () {
-	// console.log(document.getElementById("cupon-btn"));
 	cuponPrice();
 })
